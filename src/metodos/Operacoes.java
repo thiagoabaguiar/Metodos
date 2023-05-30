1ac84220eb8c2d9433db56ee78097c24f61c482b
Thiago Aguiar <thiagoabaguiar@gmail.com>
2023-05-30 15:42:45 -0300

    Add files via upload

---
+++ src/metodos/Operacoes.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metodos;
+
+/**
+ *
+ * @author Thiago Aguiar
+ */
+public class Operacoes {
+
+    public static int soma(int a, int b) {
+        
+        return a + b;
+        
+    }
+
+}
